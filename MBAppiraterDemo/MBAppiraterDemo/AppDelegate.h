9daeab0a40c2a34329523d28d4f301e7f18a22ff
Shingo Hano <kurofunekun@gmail.com>
2013-05-28 18:54:47 +0900

    Add Demo Project

---
+++ MBAppiraterDemo/MBAppiraterDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MBAppiraterDemo
+//
+//  Created by Shingo Hano on 13/05/28.
+//  Copyright (c) 2013 Shingo Hano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MBAppiraterDemo/MBAppiraterDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MBAppiraterDemo
+//
+//  Created by Shingo Hano on 13/05/28.
+//  Copyright (c) 2013 Shingo Hano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
